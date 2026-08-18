ba5cc86fc85102d924668164a153e830dd395760
Igor <korneyko2607@gmail.com>
2026-08-18 12:14:41 +0300

    create file for all markers/clean code

---
+++ detect/markers.py
@@ -0,0 +1,38 @@
+lang_markers = {
+    'Python': ['requirements.txt', '.py'],
+    'JavaScript': ['package.json', '.js'],
+    'Java': ['pom.xml', '.java'],
+    'C#': ['.csproj', '.cs'],
+    'Ruby': ['Gemfile', '.rb'],
+    'PHP': ['composer.json', '.php'],
+    'Go': ['go.mod', '.go'],
+    'Rust': ['Cargo.toml', '.rs'],
+    'C++': ['CMakeLists.txt', '.cpp', '.h'],
+    'C': ['Makefile', '.c', '.h'],
+}
+
+manifest_files = {
+    'Python': 'requirements.txt',
+    'JavaScript': 'package.json',
+    'Java': 'pom.xml',
+    'C#': '.csproj',
+    'Ruby': 'Gemfile',
+    'PHP': 'composer.json',
+    'Go': 'go.mod',
+    'Rust': 'Cargo.toml',
+    'C++': 'CMakeLists.txt',
+    'C': 'Makefile',
+}
+
+framework_markers = {
+    'Django': ['manage.py', 'settings.py'],
+    'Flask': ['app.py', 'flask'],
+    'Express': ['app.js', 'express'],
+    'Spring': ['spring'],
+    'ASP.NET': ['aspnet'],
+    'Rails': ['rails'],
+    'Laravel': ['laravel'],
+    'Symfony': ['symfony'],
+    'Gin': ['gin'],
+    'Rocket': ['rocket'],
+}
