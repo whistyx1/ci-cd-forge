584244595607bf5f50ecb8bd2d454640b88c92ca
Igor <korneyko2607@gmail.com>
2026-08-18 12:16:06 +0300

    add import

--- detect/stack_detector.py
+++ detect/stack_detector.py
@@ -1,30 +1,6 @@
 from pathlib import Path
+from detect.markers import lang_markers, manifest_files
 
-lang_markers = {
-    'Python': ['requirements.txt', '.py'],
-    'JavaScript': ['package.json', '.js'],
-    'Java': ['pom.xml', '.java'],
-    'C#': ['.csproj', '.cs'],
-    'Ruby': ['Gemfile', '.rb'],
-    'PHP': ['composer.json', '.php'],
-    'Go': ['go.mod', '.go'],
-    'Rust': ['Cargo.toml', '.rs'],
-    'C++': ['CMakeLists.txt', '.cpp', '.h'],
-    'C': ['Makefile', '.c', '.h'],
-}
-
-manifest_files = {
-    'Python': 'requirements.txt',
-    'JavaScript': 'package.json',
-    'Java': 'pom.xml',
-    'C#': '.csproj',
-    'Ruby': 'Gemfile',
-    'PHP': 'composer.json',
-    'Go': 'go.mod',
-    'Rust': 'Cargo.toml',
-    'C++': 'CMakeLists.txt',
-    'C': 'Makefile',
-}
 
 def detect_stack(path) -> dict:
     path = Path(path)
