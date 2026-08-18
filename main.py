371ea99628cabca9bdee5304a345598997ad47b8
Igor <korneyko2607@gmail.com>
2026-08-18 12:15:37 +0300

    add checks try/except

--- main.py
+++ main.py
@@ -2,4 +2,7 @@ import sys
 from detect.stack_detector  import detect_stack
 
 if __name__ == "__main__":
-    detect_stack(sys.argv[1])
+    try:
+        detect_stack(sys.argv[1])
+    except IndexError:
+        print("You entered an invalid path. Please provide a valid directory path as an argument.")
