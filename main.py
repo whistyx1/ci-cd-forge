0ca20ff17daaf8bf5d320ae757ce1d5f5af022c4
Игорь Корнейко <korneyko2607@gmail.com>
2026-08-19 16:08:26 +0300

    add detect framewrok func

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import sys
 from detect.language_detector  import detect_language
+from detect.framewrok_detect import detect_framework
 
 if __name__ == "__main__":
     try:
         detect_language(sys.argv[1])
+        detect_framework(sys.argv[1], detect_language(sys.argv[1]))
     except IndexError:
         print("You entered an invalid path. Please provide a valid directory path as an argument.")
