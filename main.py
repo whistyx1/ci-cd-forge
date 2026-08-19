237ed7fdba2c8c8a9ba23421c30ffaabb9afa9c7
Igor <korneyko2607@gmail.com>
2026-08-19 20:08:54 +0300

    call stack func

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import sys
 from detect.language_detector  import detect_language
-from detect.framewrok_detect import detect_framework
+from detect.framework_detect import detect_framework
+from detect.stack import create_stack
 
 if __name__ == "__main__":
     try:
-        detect_language(sys.argv[1])
-        detect_framework(sys.argv[1], detect_language(sys.argv[1]))
+        path = sys.argv[1]
+        stack = create_stack(path)
+        print(stack)
     except IndexError:
         print("You entered an invalid path. Please provide a valid directory path as an argument.")
