c65b18e7133f9c5f13bd94ebbe646bfa0aea2a06
Igor <korneyko2607@gmail.com>
2026-08-19 21:49:23 +0300

    set parse for requirements.txt

--- parse/parse_requirements.py
+++ parse/parse_requirements.py
@@ -1,4 +1,16 @@
-def parse_requirements(path: str) -> list[str]:
-    with open(path, "r") as f:
-        requirements = f.readlines()
-        return [req.strip() for req in requirements]
+import re
+
+def parse_requirements(content: str) -> list[str]:
+    requirements = content.splitlines()
+    stripped_requirements = [req.strip() for req in requirements if req.strip()]
+    package_names = []
+    for req in stripped_requirements:
+        if req.startswith('-r') or req.startswith('--requirement'):
+            continue
+        match = re.match(r'^[a-zA-Z0-9_.-]+', req)
+        if match:
+            package_names.append(match.group().lower())
+        else:
+            continue
+    
+    return package_names
