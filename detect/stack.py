84e6e91b65cd4845c5b34a6600167c2424d7616e
Igor <korneyko2607@gmail.com>
2026-08-19 20:11:18 +0300

    tabulation correct

--- detect/stack.py
+++ detect/stack.py
@@ -3,7 +3,8 @@ from detect.framework_detect import detect_framework
 
 
 def create_stack(path: str) -> dict:
-    return {
+    return 
+    {
         'language': detect_language(path),
         'framework': detect_framework(path, detect_language(path))
     }
