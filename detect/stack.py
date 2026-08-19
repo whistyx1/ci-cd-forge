b4f612a1e86ee9705fc674578f4000f30c87a4f9
Igor <korneyko2607@gmail.com>
2026-08-19 20:09:18 +0300

    create stack dict

--- detect/stack.py
+++ detect/stack.py
@@ -1,4 +1,10 @@
-stack = {
-    'language': None,
-    'framework': None,
-}
+from detect.language_detector import detect_language
+from detect.framework_detect import detect_framework
+
+
+def create_stack(path: str) -> dict:
+    return {
+        'language': detect_language(path),
+        'framework': detect_framework(path, detect_language(path))
+    }
+
