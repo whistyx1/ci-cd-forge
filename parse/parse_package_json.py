a2cfc782f16ce986976a1575b03e777f032c475d
Igor <korneyko2607@gmail.com>
2026-08-20 11:48:26 +0300

    delete try block for catching error

--- parse/parse_package_json.py
+++ parse/parse_package_json.py
@@ -2,12 +2,7 @@ import json
 
 def parse_package_json(content: str) -> list[str]:
     json_packages = []
-    try:
-        data = json.loads(content)
-        json_packages.extend([str(key).lower() for key in data.get('dependencies', {}).keys()])
-        json_packages.extend([str(key).lower() for key in data.get('devDependencies', {}).keys()])
-
-    except json.JSONDecodeError as e:
-        print(f"Error parsing JSON: {e}")
-        return []
+    data = json.loads(content)
+    json_packages.extend([str(key).lower() for key in data.get('dependencies', {}).keys()])
+    json_packages.extend([str(key).lower() for key in data.get('devDependencies', {}).keys()])
     return json_packages
