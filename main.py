7035f0e8994d3d58bd2b26f8a4625c2596b85b99
Igor <korneyko2607@gmail.com>
2026-08-22 22:14:24 +0300

    add indents for better output

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys
+import json
 from detect.language_detector  import detect_language
 from detect.framework_detect import detect_framework
 from detect.stack import create_stack
@@ -7,6 +8,6 @@ if __name__ == "__main__":
     try:
         path = sys.argv[1]
         stack = create_stack(path)
-        print(stack)
+        print(json.dumps(stack, indent=2))
     except IndexError:
         print("You entered an invalid path. Please provide a valid directory path as an argument.")
