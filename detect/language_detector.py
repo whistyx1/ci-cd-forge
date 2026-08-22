0c28ccb22de14def4d0c10033dda24df6f748d90
Igor <korneyko2607@gmail.com>
2026-08-22 22:38:14 +0300

    update lang detector: add detecting source file for lang

--- detect/language_detector.py
+++ detect/language_detector.py
@@ -2,16 +2,15 @@ from pathlib import Path
 from detect.markers import lang_markers
 from typing import Optional
 
+def _find_language_match(files) -> tuple[Optional[str], Optional[str]]:
+    for lang, markers in lang_markers.items():
+        for f in files:
+            for marker in markers:
+                if f.name.endswith(marker):
+                    return lang, f.name
+    return None, None
 
-def detect_language(path: str) -> Optional[str]:
+def detect_language(path: str) -> tuple[Optional[str], Optional[str]]:
     path = Path(path)
     files = list(path.iterdir())
-
-    language = None
-
-    for lang, markers in lang_markers.items():
-        if any(f.name.endswith(marker) for f in files for marker in markers):
-            language = lang
-            break
-
-    return language
+    return _find_language_match(files)
