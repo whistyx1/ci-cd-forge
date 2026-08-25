9f0f7d382ffdf1a996449af16d3a303f8bf1843a
igor <korneyko2607@gmail.com>
2026-08-25 20:53:36 +0300

    Revert "create test for csproj"
    This reverts commit 320cc15ce2eb99367a72fca05d46ba4713bc7843.

--- tests/test_project_finder.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from tempfile import TemporaryDirectory
-from pathlib import Path
-from detect.project_finder import find_projects
-
-class TestFindProject(unittest.TestCase):
-    def test_find_csproj_project(self):
-        with TemporaryDirectory() as temp_dir:
-            pass
