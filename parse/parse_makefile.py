ebc77e6364f887c9b4ae2bf2df0b75242bee3861
igor <korneyko2607@gmail.com>
2026-08-28 21:28:51 +0300

    feat: add versions to build dependencies

--- parse/parse_makefile.py
+++ parse/parse_makefile.py
@@ -1,9 +1,19 @@
 import re
 
-def parse_makefile(content: str) -> list[str]:
+from parse.dependency import Dependency
+
+
+def parse_makefile(content: str) -> list[Dependency]:
     makefile_packages = []
     for line in content.splitlines():
         line = line.strip()
         matches = re.findall(r'-l(\w[\w.-]*)', line)
-        makefile_packages.extend(lib.lower() for lib in matches)
+        for lib in matches:
+            name = lib.lower()
+            makefile_packages.append(
+                {
+                    'name': name,
+                    'version': None,
+                },
+            )
     return makefile_packages
